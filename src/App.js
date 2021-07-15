f18027c70354e192a3ba2c11d22c4ee3bc19518c
gabe-morris <gabe.e.morris@gmail.com>
2021-07-14 21:28:55 -0500

    imported data to Post and App components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./App.css";
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import Post from './Post'
+
+
 
 function App() {
+  const [post, setPost] = useState([])
+const [postId, setPostId] = useState()
+  useEffect(() =>{
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1`)
+    .then(res => {
+      console.log(res.data)
+      setPost(res.data)
+    })
+    .catch(err => console.log(err))
+   },[])
+
   return (
     <div className="App">
       <p>
